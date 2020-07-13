48185bf107349f8c8a77063d1e9955f4ae15d73c
wuzihong <developwu@126.com>
2020-07-13 12:53:32 +0800

    file of src

---
+++ src/main/java/com/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("hello")
+    public void hello() {
+        System.out.println("hello");
+    }
+}
